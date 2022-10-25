be586e9b6ae4eb44c8e9fefc4814f4c0d9e14e74
GitOsku <oskari.helenius@edu.turkuamk.fi>
2022-10-25 21:45:22 +0300

    Yeet
    Ja taas mennää

--- Osa1/Task1.1-1.2/task1/src/App.js
+++ Osa1/Task1.1-1.2/task1/src/App.js
@@ -29,12 +29,24 @@ const Header = (props) => {
 
 
 const Content = (props) => {
+
   console.log(props.excercises1)
+
   return(
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+ 
+  )
+}
+
+const Part = (props) => {
+  console.log(props.excercises)
+  return(
+    <div>
+      <p>{props.part} {props.exercises} </p>
     </div>
   )
 }
