d70c62edbc79f40fab449898376e92b4dab14fdb
GitOsku <oskari.helenius@edu.turkuamk.fi>
2022-10-26 15:33:38 +0300

    1.4
    1.4

--- Osa1/Task1.1-1.2/task1/src/App.js
+++ Osa1/Task1.1-1.2/task1/src/App.js
@@ -1,24 +1,28 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 ={
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 ={
-    name: 'State of a component',
-    exercises: 14
-  }
-  return (
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+
+    {
+      name: 'Using props to pass data',
+      exercises: 8
+    },
+
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+
+  ]
+ return (
     <div>
       <Header course= {course} />
-      <Part name={part1.name} exercises={part1.exercises} />
-      <Part name={part2.name} exercises={part2.exercises} />
-      <Part name={part3.name} exercises={part3.exercises} />
-    </div>
+      <Content parts= {parts} />
+      <Total parts = {parts} />
+      </div>
   )
 }
 
@@ -37,6 +41,27 @@ const Part = (props) => {
     <div>
       <p>{props.name} {props.exercises}</p>
     </div>
+  )
+
+}
+
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return(
+  <div>
+    <p>Total of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  </div>
   )
 }
 export default App
