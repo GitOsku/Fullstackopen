cf09cb249689c3a5dfbf1e3ecfc1c8281dd86d84
GitOsku <oskari.helenius@edu.turkuamk.fi>
2022-10-27 16:33:16 +0300

    1.5
    1.5

--- Osa1/Task1.1-1.2/task1/src/App.js
+++ Osa1/Task1.1-1.2/task1/src/App.js
@@ -1,67 +1,32 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
 
-    {
-      name: 'Using props to pass data',
-      exercises: 8
-    },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
 
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-
-  ]
- return (
-    <div>
-      <Header course= {course} />
-      <Content parts= {parts} />
-      <Total parts = {parts} />
-      </div>
-  )
-}
-
-const Header = (props) => {
-  console.log(props)
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
   return(
     <div>
-      <h1>{props.course}</h1>
+      <h1>{course.name}</h1>
+      <p> {course.parts[0].name} {course.parts[0].exercises}</p>
+      <p> {course.parts[1].name} {course.parts[1].exercises}</p>
+      <p> {course.parts[2].name} {course.parts[2].exercises}</p>
     </div>
   )
-}
+}  
 
-const Part = (props) => {
-  console.log(props)
-  return(
-    <div>
-      <p>{props.name} {props.exercises}</p>
-    </div>
-  )
-
-}
-
-const Content = (props) => {
-  console.log(props)
-  return(
-    <div>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  return(
-  <div>
-    <p>Total of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  </div>
-  )
-}
 export default App
