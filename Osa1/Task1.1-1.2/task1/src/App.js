3777fac98f22aaef12765014a2011e183a9f55ff
GitOsku <oskari.helenius@edu.turkuamk.fi>
2022-11-04 22:10:56 +0200

    1.7
    1.7

--- Osa1/Task1.1-1.2/task1/src/App.js
+++ Osa1/Task1.1-1.2/task1/src/App.js
@@ -10,7 +10,7 @@ const Header = (props) => {
     
   )
 }
-
+//lower header
 const Header2 = (props) => {
   console.log(props)
   return(
@@ -20,35 +20,54 @@ const Header2 = (props) => {
   )
 }
 
+//good stats
 const GStats =(props) => {
   console.log(props)
   return(
-    // ei perkele toimi
     <div>
       <p>good {props.good}</p> 
     </div>
   )
 }
 
+//neutral stats
 const NStats =(props) => {
   console.log(props)
   return(
-    // ei perkele toimi
     <div>
       <p>neutral {props.neutral}</p>
     </div>
   )
 }
-
+//Bad stats
 const BStats =(props) => {
   console.log(props)
   return(
-    // ei perkele toimi
     <div>
       <p>bad {props.bad}</p>
     </div>
   )
 }
+
+// Shows all reviews
+const Stats =(props) => {
+  console.log(props)
+  return(
+    <div>
+      <p>all {props.all} </p>
+    </div>
+  )
+}
+
+const Average =(props) => {
+  return(
+    <div>
+      <p>average {props.avg / props.total} </p>
+    </div>
+  )
+}
+
+
 
 const App = () => {
   // tallenna napit toiseen kohtaan
@@ -62,21 +81,38 @@ const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [bad, setBad,] = useState(0)  
+  const [allReviews, setAll] = useState(0)
+  const [total, setTotal] = useState(0)
 
   return(
     <div>
       <Header name ={feedback.name} />
 
-      <button onClick={() => setGood(good + 1)}>
+      <button onClick={() =>
+        {setGood(good + 1); 
+        setAll(allReviews + 1);
+        setTotal(total + 1);
+      }}
+      > 
         good
       </button>
 
-      <button onClick={() =>setNeutral(neutral + 1)}>
+      <button onClick={() =>
+        {setNeutral(neutral + 1);
+        setAll(allReviews + 0);
+        setTotal(total + 1);
+      }}
+      >
         neutral
       </button>
 
-      <button onClick={() => setBad(bad +1)}>
+      <button onClick={() => 
+        {setBad(bad + 1);
+        setAll(allReviews + -1);
+        setTotal(total + 1);
+        }}
+        >
         bad
       </button>
 
@@ -84,8 +120,9 @@ const App = () => {
       <GStats good ={good}  />
       <NStats neutral ={neutral}  />
       <BStats bad ={bad}  />
+      <Stats all ={good + neutral + bad}/>
+      <Average avg = {allReviews} total ={total}/>
     </div>
   )
 }
-
 export default App
