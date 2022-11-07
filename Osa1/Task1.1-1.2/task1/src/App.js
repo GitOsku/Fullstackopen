12da84372577e73526f67ec1ece6d4a25dc9be43
GitOsku <oskari.helenius@edu.turkuamk.fi>
2022-11-07 20:12:10 +0200

    Moved button and made Statisticsline
    Moved button and made Statisticsline
    Also added table and tr

--- Osa1/Task1.1-1.2/task1/src/App.js
+++ Osa1/Task1.1-1.2/task1/src/App.js
@@ -19,6 +19,24 @@ const StatsHeader = () => {
   )
 }
 
+const Button = ({handleClick, text}) => {
+  return(
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const StatisticsLine = (props) => {
+  console.log(props)
+  return(
+    <tr>
+      {props.text} {props.value}
+    </tr>
+  )
+
+}
+
 const Statistics = (props) => {
   console.log(props)
   if (props.total === 0) 
@@ -29,60 +47,60 @@ const Statistics = (props) => {
 }
 else {
   return(
-      <div>
-        <p>good {props.good} </p>
-        <p>neutral {props.neutral} </p>
-        <p>bad {props.bad}</p>
-        <p>all {props.all}</p>
-        <p>average {props.avg / props.total}</p>
-      </div>
+      <table>
+        <tbody>
+        <StatisticsLine text="good" value ={props.good} />
+        <StatisticsLine text="neutral" value ={props.neutral} />
+        <StatisticsLine text ="bad" value ={props.bad} />
+        <StatisticsLine text ="all" value ={props.all}/>
+        <StatisticsLine text ="average" value ={props.ar / props.total}/>
+        </tbody>
+      </table>
     )
   }
 }
 
-const App = () => {
-  // tallenna napit toiseen kohtaan
 
+
+const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad,] = useState(0)  
-  const [allReviews, setAll] = useState(0)
-  const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState (0)
+  const [allReviews, setAll] = useState (0)
+
+  const goodHandle = () => {
+    setGood(good +1);
+    setAll(allReviews + 1);
+    setTotal(total + 1)
+  }
+
+  const neutralHandle = () => {
+    setNeutral(neutral +1);
+    setAll(allReviews + 0);
+    setTotal(total + 1)
+  }
+
+  const badHandle = () => {
+    setBad(bad +1);
+    setAll(allReviews + -1);
+    setTotal(total + 1)
+  }
 
   return(
     <div>
       <Header/>
-
-      <button onClick={() =>
-        {setGood(good + 1); 
-        setAll(allReviews + 1);
-        setTotal(total + 1);
-      }}
-      > 
-        good
-      </button>
-
-      <button onClick={() =>
-        {setNeutral(neutral + 1);
-        setAll(allReviews + 0);
-        setTotal(total + 1);
-      }}
-      >
-        neutral
-      </button>
-
-      <button onClick={() => 
-        {setBad(bad + 1);
-        setAll(allReviews + -1);
-        setTotal(total + 1);
-        }}
-        >
-        bad
-      </button>
-
+      <div>
+      <Button handleClick={goodHandle} text ='good' />
+      <Button handleClick={neutralHandle} text ='neutral' />
+      <Button handleClick={badHandle} text ='bad' />
+      </div>
       <StatsHeader/>
-      <Statistics good ={good} neutral = {neutral} bad = {bad} all = {good + neutral + bad} avg = {allReviews} total ={total}/>
+      <Statistics good ={good} neutral = {neutral} bad = {bad} 
+      all = {good + neutral + bad} 
+      ar = {allReviews} // var that defines numeric value to good neutral and bad review
+      total ={total} />
     </div>
   )
 }
