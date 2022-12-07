6ab34d0adf5e1d627801243ddd70e72d9371ddf3
GitOsku <oskari.helenius@edu.turkuamk.fi>
2022-12-07 17:49:51 +0200

    Osa 2 part 2.1
    Osa 2 part 2.1

--- Osa1/Task1.1-1.2/task1/src/App.js
+++ Osa1/Task1.1-1.2/task1/src/App.js
@@ -1,67 +1,12 @@
+import Course from "./Course"
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-
-    {
-      name: 'Using props to pass data',
-      exercises: 8
-    },
-
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-
-  ]
- return (
-    <div>
-      <Header course= {course} />
-      <Content parts= {parts} />
-      <Total parts = {parts} />
-      </div>
-  )
-}
-
-const Header = (props) => {
-  console.log(props)
-  return(
-    <div>
-      <h1>{props.course}</h1>
+  return( 
+    <div> 
+      <Header course = {course} />
     </div>
   )
 }
 
-const Part = (props) => {
-  console.log(props)
-  return(
-    <div>
-      <p>{props.name} {props.exercises}</p>
-    </div>
-  )
 
-}
-
-const Content = (props) => {
-  console.log(props)
-  return(
-    <div>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  return(
-  <div>
-    <p>Total of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  </div>
-  )
-}
 export default App
